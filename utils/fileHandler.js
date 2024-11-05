82edecd53ef92b6e4a3fedcc3867911bc83eb6ac
Bile6 <bileabdulkadirmohamed@gmail.com>
2024-11-05 11:01:37 +0300

    Adding Tasks JSON

--- utils/fileHandler.js
+++ utils/fileHandler.js
@@ -3,12 +3,10 @@ exports.writeTasksToFile= (data)=>{
     fs.writeFileSync('/../data/tasks.json',JSON.stringify(data, null, 2))
     return data;
 }
-
 exports.writeTasksToFile = () =>{
     if(!fs.existsSync('/../data/tasks.json')){
         this.writeTasksToFile([]);
     }
     fs.readFileSync('./../data/tasks.json')
     return JSON.parse(data);
-   
 }
